45e8633c82de9912fc43dee40f42b894cf38edec
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2023-12-10 19:25:18 -0500

    actualizacion 3 dias

--- precache-manifest.1ee451be515c68c4049975a21dbae498.js
+++ precache-manifest.1ee451be515c68c4049975a21dbae498.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "755a526f6c88957d106ad5faf1b7e370",
+    "revision": "f06b113d1d1f9829f0c53a4c18559c12",
     "url": "/index.html"
   },
   {
-    "revision": "9326d6ee53b528950b05",
-    "url": "/static/js/2.403b2f96.chunk.js"
+    "revision": "2188ce322b0e5d53295f",
+    "url": "/static/js/2.47cbba43.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/static/js/2.403b2f96.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.47cbba43.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "29179e5197765e36f680",
-    "url": "/static/js/main.5073a7ec.chunk.js"
+    "revision": "0c240495e501594a7b44",
+    "url": "/static/js/main.9144e805.chunk.js"
   },
   {
     "revision": "e86c9e0c6088f7080823",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9fa0c6d147c5e8f28b94774b749497c5.js"
+  "/precache-manifest.1ee451be515c68c4049975a21dbae498.js"
 );
 
 self.addEventListener('message', (event) => {
