3a7763300e0618ec440626ce341569d6d877e9d9
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2024-01-24 11:59:13 -0500

    updatando

---
+++ precache-manifest.bf1f23adacc0f059255c6cf210def5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026d4dac2a1725315e5561c9ce07fe80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1f65c7f7391e683a17",
+    "url": "/static/js/2.d2cccc60.chunk.js"
+  },
+  {
+    "revision": "57fcd7494da9c895f301273981702744",
+    "url": "/static/js/2.d2cccc60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d59be967811dc41970",
+    "url": "/static/js/main.6aac66c1.chunk.js"
+  },
+  {
+    "revision": "444a0910a50d246d9ce4",
+    "url": "/static/js/runtime-main.6b408e45.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b75d895334b83406671d35fb27c1340.js"
+  "/precache-manifest.bf1f23adacc0f059255c6cf210def5e5.js"
 );
 
 self.addEventListener('message', (event) => {
